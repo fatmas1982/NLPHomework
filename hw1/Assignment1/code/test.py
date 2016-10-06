e7a3a0042acf284217a44213b5e20d1262a93ac0
Troy Whorten <sabotuer99@gmail.com>
2016-10-06 04:49:11 +0000

    ready for submit

--- hw1/Assignment1/code/test.py
+++ hw1/Assignment1/code/test.py
@@ -6,17 +6,18 @@ from featureextractor import FeatureExtractor
 from transition import Transition
 
 if __name__ == '__main__':
-    data = dataset.get_swedish_train_corpus().parsed_sents()
+    data = dataset.get_english_train_corpus().parsed_sents()
     random.seed(1234)
     subdata = random.sample(data, 200)
 
     try:
-        # tp = TransitionParser(Transition, FeatureExtractor)
-        # tp.train(subdata)
-        # tp.save('swedish.model')
+        tp = TransitionParser(Transition, FeatureExtractor)
+        tp.train(subdata)
+        tp.save('english.model')
 
-        testdata = dataset.get_swedish_test_corpus().parsed_sents()
-        tp = TransitionParser.load('badfeatures.model')
+        testdata = dataset.get_english_test_corpus().parsed_sents()
+        #        tp = TransitionParser.load('badfeatures.model')
+
 
         parsed = tp.parse(testdata)
 
